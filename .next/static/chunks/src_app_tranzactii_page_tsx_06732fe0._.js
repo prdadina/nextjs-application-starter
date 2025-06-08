b3956abfee5aa0f25d076e05d6dec1a60723c319
prdadina <predaadinaa@gmail.com>
2025-06-08 14:22:00 +0300

    Add .next/static/chunks/src_app_tranzactii_page_tsx_06732fe0._.js 1749381720867

---
+++ .next/static/chunks/src_app_tranzactii_page_tsx_06732fe0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b13ae226._.js",
+  "static/chunks/node_modules_2d58e1d8._.js"
+],
+    source: "dynamic"
+});
